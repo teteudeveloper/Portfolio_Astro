a3a3a30645740c3423f9cdb2410dd892a99c1649
teteudeveloper <teteudeveloper@gmail.com>
2026-01-09 20:09:20 -0300

    fix: add a new adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -10,6 +9,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
+  adapter: vercel(),
 
   integrations: [react()]
 });
